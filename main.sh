eb7c76c1745e6d356443851399f8d92d2f91ccc8
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-05-27 14:59:12 +0545

    Refactor project management script
    - Removed deletion of tasks.json and directory listing in check_projects function to retain existing tasks and simplify output.
    - Removed unnecessary file existence check and old_data handling in add_project function for cleaner and more straightforward code.
    - Simplified JSON task addition by appending new task data directly to tasks.json.

--- main.sh
+++ main.sh
@@ -3,6 +3,8 @@
 check_projects(){
   echo "Checking the list of projects"
   echo "--------------------------------"
+  rm -f tasks.json
+  ls -lah
 
   echo "--------------------------------"
 }
@@ -17,18 +19,27 @@ remove_project(){
 add_project(){
   echo "Add a project"
   echo "--------------------------------"
+
   
   read -p "Enter the name of the project: " name
   read -p "Enter the description of the project: " description
   
+  if [ ! -f tasks.json ]; then
+    touch tasks.json
+  fi
 
+  old_data=$(cat tasks.json)
   
   task_data=$(jq -n \
   --arg name "$name" \
   --arg description "$description" \
   '{name: $name, description: $description}')
 
-  echo $task_data >> tasks.json
+  if [ -z "$old_data" ]; then
+    echo "[$task_data]" > tasks.json
+  else
+    echo "$old_data" | jq ". + [$task_data]" > tasks.json
+  fi
 
   echo "Project added successfully"
   echo "--------------------------------"
