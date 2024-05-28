5d31d4b63da8580099af0789896f581e2a052bed
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-05-28 20:50:50 +0545

    The file can now remove the tasks from the lsit

--- remove.sh
+++ remove.sh
@@ -3,4 +3,22 @@
 echo "Removing a project"
 echo "--------------------------------"
 
+length=$(jq '. | length' tasks.json)
+
+for i in $(seq 0 $((length - 1))); do
+  name=$(jq -r ".[$i].name" tasks.json)
+  echo "$((i+1)): $name"
+done
+
+echo "--------"
+
+read -p "Enter the number of the project you want to remove: " number
+
+if [ $number -gt $length ]; then
+  echo "Invalid number"
+  exit 1
+fi
+# Remove the selected project
+jq "del(.[$((number - 1))])" tasks.json > temp.json && mv temp.json tasks.json
+
 echo "--------------------------------"
