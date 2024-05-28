ccf10d9aa39a6df4cb5ed1fcb72395e964374249
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-05-28 20:38:37 +0545

    It is possible to add status to the projects

--- check.sh
+++ check.sh
@@ -10,9 +10,9 @@
     exit 0
   fi
 
-  lentgh=$(jq '. | length' tasks.json)
+  length=$(jq '. | length' tasks.json)
 
-  for i in $(seq 0 $((lentgh - 1))); do
+  for i in $(seq 0 $((length - 1))); do
     name=$(jq -r ".[$i].name" tasks.json)
     echo "$((i+1)): $name"
   done
@@ -22,12 +22,13 @@
   echo "Select the operation you want to perform"
   echo "c. Check a project in detail"
   echo "u. Update the status of a project"
-  echo "e. Exit Priotity"
+  echo "e. Exit Priority"
 
   echo "-------"
   read -p "Enter your choice: " choice
 
   case $choice in
+      # TODO: Implement the update functionality
     c) 
       echo "enter the number of the project you want to check"
       read chosenNumber;
@@ -35,14 +36,33 @@
       projectNumber=$((chosenNumber-1)) 
       name=$(jq -r ".[$projectNumber].name" tasks.json)
       description=$(jq -r ".[$projectNumber].description" tasks.json)
-  
+      status=$(jq -r ".[$projectNumber].status" tasks.json)  
+
       echo "Project name: $name"
       echo "Project description: $description"
-
+      echo "Project status: $status"
       ;;
     u)
-      
-      echo "update"
+      # TODO: Implement the update functionality
+      echo "Enter the number of the project you want to check"
+      read chosenNumber
+
+      projectNumber=$((chosenNumber-1))
+      name=$(jq -r ".[$projectNumber].name" tasks.json)
+      description=$(jq -r ".[$projectNumber].description" tasks.json)
+      status=$(jq -r ".[$projectNumber].status" tasks.json)
+
+      echo "Project Name: $name"
+      echo "Description: $description"
+      echo "Current Status: $status"
+      read -p "What is the current status? " current_status
+
+      # Update the status in the tasks.json file
+      jq --argjson index "$projectNumber" --arg status "$current_status" '
+        .[$index].status = $status
+      ' tasks.json > temp.json && mv temp.json tasks.json
+
+      echo "Status updated successfully!"
       ;;
     e)
       echo "exiting"
