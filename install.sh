b22ab63ca28ff5b07ba2e0950cad6151437b8f4f
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-06-01 20:46:47 +0545

    Refactor on the installation script
    Checks if the jq actually exists or not, before isntalling it

--- install.sh
+++ install.sh
@@ -3,7 +3,12 @@
 filedir="$HOME/alex/scripts/priority"
 
 echo " I N S T A L L I N G    D E P E N D E N C I E S "
-sudo pacman -Syu jq
+if pacman -Qi jq &>/dev/null; then
+  echo "jq is already installed"
+else
+  echo "installing jq"
+  sudo pacman -S jq
+fi
 
 echo " C R E A T I N G   D I R E C T O R I E S "
 if [ ! -d $filedir ]; then
@@ -11,17 +16,17 @@ if [ ! -d $filedir ]; then
 fi
 
 echo " C R E A T I N G   S Y M L I N K "
-ln $PWD/add.sh $filedir/add.sh 
+ln $PWD/add.sh $filedir/add.sh
 ln $PWD/main.sh $filedir/main.sh
 ln $PWD/check.sh $filedir/check.sh
 ln $PWD/remove.sh $filedir/remove.sh
 
 echo "C R E A T I N G   T A S K S . J S O N"
-touch $filedir/tasks.json 
+touch $filedir/tasks.json
 
 echo "C R E A T I N G   A L I A S"
-echo "alias priority=$filedir/main.sh" >> ~/.zshrc 
-echo "alias priority=$filedir/main.sh" >> ~/.bashrc
+echo "alias priority=$filedir/main.sh" >>~/.zshrc
+echo "alias priority=$filedir/main.sh" >>~/.bashrc
 
 echo "I N S T A L L A T I O N   C O M P L E T E"
 echo "-----------------------------------------"
